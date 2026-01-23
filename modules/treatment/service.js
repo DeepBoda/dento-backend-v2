f1166d9f08e8fc15426ac9c770a3552b895bb4b8
DeepBoda <deepboda18@gmail.com>
2026-01-23 16:00:00 +0530

    feat: treatment protocol with comprehensive tests

--- modules/treatment/service.js
+++ modules/treatment/service.js
@@ -1,4 +1,5 @@
 const Treatment = require("./model");
+const { Op } = require("sequelize");
 
 exports.create = async (data) => {
   return Treatment.create(data);
@@ -7,6 +8,11 @@ exports.create = async (data) => {
 exports.get = async (condition) => {
   return Treatment.findAll(condition);
 };
+
+exports.findOne = async (condition) => {
+  return Treatment.findOne(condition);
+};
+
 exports.getSum = async (condition) => {
   return Treatment.sum(condition);
 };
@@ -21,4 +27,28 @@ exports.remove = async (condition) => {
 
 exports.sum = async (data, query) => {
   return await Treatment.sum(data, query);
+};
+
+exports.getByStatus = async (treatmentPlanId, status) => {
+  return Treatment.findAll({
+    where: { treatmentPlanId, status },
+    order: [["createdAt", "ASC"]],
+  });
+};
+
+exports.getTreatmentStats = async (patientId) => {
+  const [total, completed, pending] = await Promise.all([
+    Treatment.count({
+      include: [{ association: "treatmentPlan", where: { patientId }, attributes: [] }],
+    }),
+    Treatment.count({
+      where: { status: "completed" },
+      include: [{ association: "treatmentPlan", where: { patientId }, attributes: [] }],
+    }),
+    Treatment.count({
+      where: { status: { [Op.in]: ["pending", "in-progress"] } },
+      include: [{ association: "treatmentPlan", where: { patientId }, attributes: [] }],
+    }),
+  ]);
+  return { total, completed, pending };
 };
